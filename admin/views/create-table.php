8850b46101fa3d0e02dbbe51e3e23662b68cc846
Anja Kammer <post@anjakammer.de>
2017-01-04 18:58:46 +0100

    refactored HTML

--- admin/views/create-table.php
+++ admin/views/create-table.php
@@ -19,7 +19,7 @@
 	var ajaxurl = '<?php echo admin_url( 'admin-ajax.php' ); ?>';
 </script>
 
-<?
+<?php
 if ( ! current_user_can( 'manage_database' ) ) {
 	wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 }
@@ -33,12 +33,10 @@ if ( ! current_user_can( 'manage_database' ) ) {
 		<li class="tab-link" data-tab="tab-sql"><?php _e( 'SQL Editor', 'd2t' ); ?></li>
 	</ul>
 
-
-	<div id="tab-gui" class="tab-content current" role="form" data-toggle="validator">
+	<div id="tab-gui" class="tab-content current" role="form">
 		<div id="meta-data">
-			<input type="text" name="table_name" id="table_name" size="40" maxlength="64" value=""
-			       placeholder="table_name" autofocus="" required
-			       data-error="can not be empty" class="form-control"/>
+			<input type="text" id="table_name" size="40" maxlength="64" value=""
+			       placeholder="table_name" autofocus="" required/>
 		</div>
 
 		<table id="columns" class="table table-striped">
@@ -54,12 +52,12 @@ if ( ! current_user_can( 'manage_database' ) ) {
 			</tr>
 			<tr>
 				<td></td>
-				<td><input value="id" disabled class="field_type form-control" ></td>
-				<td><input value="INT" disabled class="field_type form-control" ></td>
-				<td><input value="autoincrement" disabled class="field_type form-control" ></td>
-				<td><input type="checkbox" disabled class="field_type form-control" ></td>
-				<td><input type="checkbox" selected disabled class="field_type form-control" ></td>
-				<td><input value="automatically included" disabled class="field_type form-control" ></td>
+				<td><input value="id" disabled class="field_type form-control"></td>
+				<td><input value="int" disabled class="field_type form-control"></td>
+				<td><input value="autoincrement" disabled class="field_type form-control"></td>
+				<td><input type="checkbox" disabled class="field_type form-check"></td>
+				<td><input type="checkbox" checked disabled class="field_type form-check"></td>
+				<td><input value="automatically included" disabled class="field_type form-control"></td>
 			</tr>
 
 			<?php
@@ -70,14 +68,13 @@ if ( ! current_user_can( 'manage_database' ) ) {
 						<button class="btn btn-danger btn-sm delete-column" data-column="<?php echo $i ?>">X</button>
 					</td>
 					<td><!-- column name -->
-						<input id="field_name_<?php echo $i ?>" type="text" name="field_name_<?php echo $i ?>"
-						       data-column="<?php echo $i ?>"
+						<input id="field_name_<?php echo $i ?>" type="text" data-column="<?php echo $i ?>"
 						       maxlength="64" class="field_name form-control" title="Column" size="10"
 						       value="" placeholder="column_name" required>
 					</td>
 					<td><!-- column type -->
-						<select class="field_type form-control" name="field_type_<?php echo $i ?>"
-						        id="field_type_<?php echo $i ?>" data-column="<?php echo $i ?>">
+						<select class="field_type form-control" id="field_type_<?php echo $i ?>"
+						        data-column="<?php echo $i ?>">
 							<option
 								title="A 4-byte integer, signed range is -2,147,483,648 to 2,147,483,647, unsigned range is 0 to 4,294,967,295">
 								int
@@ -96,29 +93,27 @@ if ( ! current_user_can( 'manage_database' ) ) {
 						</select>
 					</td>
 					<td><!-- column default -->
-						<select name="field_default_type_<?php echo $i ?>" id="field_default_type_<?php echo $i ?>"
+						<select id="field_default_type_<?php echo $i ?>"
 						        class="field_default_type form-control" data-column="<?php echo $i ?>">
 							<option value="NONE" title="Empty field value">None</option>
 							<option value="USER_DEFINED" title="Click to define">As defined:</option>
 							<option value="NULL" title="Missing or not existent data">NULL</option>
 							<option value="CURRENT_TIMESTAMP" title="Format: YYYY-MM-DD">CURRENT_TIMESTAMP</option>
 						</select>
-						<input type="text" name="field_default_value_<?php echo $i ?>"
-						       id="field_default_value_<?php echo $i ?>" class="field_default_value form-control"
+						<input type="text" id="field_default_value_<?php echo $i ?>"
+						       class="field_default_value form-control"
 						       data-column="<?php echo $i ?>" size="12" value="" style="display: none;">
 					</td>
 					<td><!-- column NULL -->
-						<input name="field_allow_null_<?php echo $i ?>" id="field_allow_null_<?php echo $i ?>"
-						       data-column="<?php echo $i ?>" type="checkbox"
-						       value="NULL" class="field_allow_null form-control">
+						<input id="field_allow_null_<?php echo $i ?>" data-column="<?php echo $i ?>" type="checkbox"
+						       value="NULL" class="field_allow_null form-check">
 					</td>
 					<td>
-						<input name="field_is_unique_<?php echo $i ?>" id="field_is_unique_<?php echo $i ?>"
-						       data-column="<?php echo $i ?>" type="checkbox" value="UNIQUE"
-						       class="is_unique form-control"></td>
+						<input id="field_is_unique_<?php echo $i ?>" data-column="<?php echo $i ?>" type="checkbox"
+						       value="UNIQUE" class="is_unique form-check">
+					</td>
 					<td><!-- column comments -->
-						<input type="text" name="field_comments_<?php echo $i ?>" id="field_comments_<?php echo $i ?>"
-						       size="12" maxlength="1024" value=""
+						<input type="text" id="field_comments_<?php echo $i ?>" size="12" maxlength="1024" value=""
 						       class="field_comments form-control" data-column="<?php echo $i ?>" placeholder="comment">
 					</td>
 				</tr>
@@ -128,7 +123,7 @@ if ( ! current_user_can( 'manage_database' ) ) {
 		<!-- TODO the column_id is not incremented - so the saving will not work -->
 		<!--		<button type="button" class="btn btn-outline-success add-column">Add column</button>-->
 		<form id="sql-creator-form">
-			<?php wp_nonce_field( 'd2t_run_sql_statement' ); ?>
+			<?php wp_nonce_field( 'd2t_create_sql_statement' ); ?>
 			<textarea rows="10" name="sql_from_creator" id="sql_from_creator" class="form-control" dir="ltr"
 			          style="display: none;"></textarea>
 			<div class="form-group">
@@ -153,7 +148,7 @@ if ( ! current_user_can( 'manage_database' ) ) {
 			<strong><?php _e( 'Separate Multiple Statements With A New Line', 'd2t' ); ?></strong><br/>
 		</div>
 		<form id="sql-statement-form">
-			<?php wp_nonce_field( 'd2t_create_sql_statement' ); ?>
+			<?php wp_nonce_field( 'd2t_run_sql_statement' ); ?>
 			<div class="form-group">
 					<textarea rows="10" name="sql_statement" id="sql_statement" class="form-control" dir="ltr"
 					          placeholder="CREATE TABLE table_name ..."></textarea>
@@ -170,6 +165,4 @@ if ( ! current_user_can( 'manage_database' ) ) {
 			</div>
 		</form>
 	</div>
-
-
 </div>
